300af6885c68128fd4c8eac4de816c2841c45da8
namuecores <namuecores@gmail.com>
2026-05-28 13:38:22 +0900

    웹 크롤링 연습2

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -13,11 +13,11 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 # 파일에 저장
-f = open("clien.txt", "wt", encoding="utf-8")
+f = open("todayHumor.txt", "wt", encoding="utf-8")
 
 #url = "https://www.clien.net/service/board/sold"
-for i in range(0,10):
-    url = "https://www.clien.net/service/board/sold?&od=T31&category=0&po=" + str(i)
+for i in range(1, 11):
+    url = "https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=" + str(i)
     print(url)
 
     #data = urllib.request.urlopen(url).read()
@@ -27,16 +27,23 @@ for i in range(0,10):
 
     soup = BeautifulSoup(data, 'html.parser')
     # 필터링 작업
-    list = soup.find_all("span", {"data-role":"list-title-text"})
+    list = soup.find_all("td", attrs={"class":"subject"})
     for tag in list:
-        title = tag.text.strip()
-        # 문자열 검색(정규표현식)
-        if re.search("아이폰", title):
+        title = tag.find("a").text.strip()
+        # print(title)
+        # f.write(title + "\n")
+        #문자열 검색(정규표현식)
+        if re.search("한국", title):
             print(title)
             f.write(title + "\n")
 
 f.close()
 
+# <td class="subject">
+# <a href="/board/view.php?table=bestofbest&amp;no=482962&amp;s_no=482962&amp;page=1" target="_top">
+# "요즘 시대에서 한국인을 나누는 기준"</a>
+# <span class="list_memo_count_span"> [15]</span>  
+# <span style="margin-left:4px;"><img src="//www.todayhumor.co.kr/board/images/list_icon_photo.gif" style="vertical-align:middle; margin-bottom:1px;"> </span><img src="//www.todayhumor.co.kr/board/images/list_icon_shovel.gif?2" alt="펌글" style="margin-right:3px;top:2px;position:relative"> <span style="color:#999">5일</span></td>
 
 # 선택한 블럭 주석 : ctrl + /
 # <span class="subject_fixed" data-role="list-title-text" title="미개봉) 아이폰 Xs 데빌케이스 Type X2">
